231b890f1247178c5050b341acd51dfe5f20ab6b
Oleg <shevchenko.oleg.87@gmail.com>
2022-01-31 22:25:41 +0300

    01 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
 import React from 'react';
 import './App.css';
+import TodoList, {TaskType} from "./TodoList";
+
+let whatToLearnTasks: Array<TaskType> = [
+    {
+        id: 1,
+        title: 'Redux',
+        isDone: true
+    },
+    {
+        id: 2,
+        title: 'React',
+        isDone: false
+    },
+    {
+        id: 3,
+        title: 'JS',
+        isDone: true
+    }
+]
+
+let whatToReadTasks: Array<TaskType> = [
+    {
+        id: 1,
+        title: 'JS for children',
+        isDone: true
+    },
+    {
+        id: 2,
+        title: 'HTML/CSS',
+        isDone: false
+    },
+    {
+        id: 3,
+        title: 'War and Peace',
+        isDone: false
+    }
+]
+
+let whatToByTasks: Array<TaskType> = [
+    {
+        id: 1,
+        title: 'Milk',
+        isDone: false
+    },
+    {
+        id: 2,
+        title: 'Bread',
+        isDone: false
+    },
+    {
+        id: 3,
+        title: 'Meat',
+        isDone: true
+    }
+]
+
+
 
 function App() {
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList title={'What to learn'} tasks={whatToLearnTasks}/>
+            <TodoList title={'What to read'} tasks={whatToReadTasks}/>
+            <TodoList title={'What to by'} tasks={whatToByTasks}/>
         </div>
     );
 }
---
+++ src/Task.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {TaskType} from "./TodoList";
+
+type TaskPropsType = {
+    tasks: Array<TaskType>
+}
+
+const Task = (props: TaskPropsType) => {
+    return (
+        <div>
+            <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+        </div>
+    );
+};
+
+export default Task;
